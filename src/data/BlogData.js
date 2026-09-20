77f089766fb8b0eae66dcc4f99c982a39f9ec7e4
l0gicr <abdulrhmanyalrefaai@gmail.com>
2026-09-20 14:51:13 +0300

    Fix duplicate name property in BlogData
    Removed duplicate 'name' property from blog entry.

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -3,8 +3,7 @@
 export const Blogs = [
     {
         id:1,
-        name:"My Photography Gallery",
-name: "My Photography Gallery",
+       name: "My Photography Gallery",
 description: "My Photography Gallery",        tags:["Photos", "Life", "Art"],
         date:"12 May, 2026",
         //imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
